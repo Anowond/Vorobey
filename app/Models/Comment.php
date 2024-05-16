e7004ce479748891090ef85bb25a8531337c885e
Anowond <baroux.maxime2@gmail.com>
2024-05-16 23:16:46 +0200

    ménage

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -26,13 +26,4 @@ class Comment extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function product(): BelongsTo
-    {
-        return $this->belongsTo(Product::class);
-    }
-
-    public function sound(): BelongsTo
-    {
-        return $this->belongsTo(Sound::class);
-    }
 }
